d6bc3d281959f4bbaeb4052c295206e032b83432
lasse9000dk <lasse9000dk@gmail.com>
2018-09-05 09:56:55 +0200

    asdfasdf

--- PySNMP_test.py
+++ PySNMP_test.py
@@ -5,23 +5,12 @@ errorIndication, errorStatus, errorIndex, varBinds = next(
            CommunityData('H5'),
            UdpTransportTarget(('192.168.1.5', 161)),
            ContextData(),
-           #ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)),
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1')))
+           ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)),
+           ObjectType(ObjectIdentity('IF-MIB', 'ifNumber', 0)),
+           ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')))
 )
-'''varBinds = [
-    ('1.3.6.1.2.1.1.3.0', 12345),
-    ('1.3.6.1.6.3.1.1.4.1.0', '1.3.6.1.6.3.1.1.5.2'),
-    ('1.3.6.1.6.3.18.1.3.0', '0.0.0.0'),
-    ('1.3.6.1.6.3.18.1.4.0', ''),
-    ('1.3.6.1.6.3.1.1.4.3.0', '1.3.6.1.4.1.20408.4.1.1.2'),
-    ('1.3.6.1.2.1.1.1.0', 'my system')
-]
 
-varBinds = [rfc1902.ObjectType(rfc1902.ObjectIdentity(x[0]), x[1]).resolveWithMib(mibViewController) for x in varBinds]
 
-for varBind in varBinds:
-    print(varBind.prettyPrint())
-'''
 if errorIndication:
 
 
