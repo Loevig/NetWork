ff2386f3c15d78e70c5e34c31c7c83089f2c380f
lasse9000dk <lasse9000dk@gmail.com>
2018-09-05 11:13:03 +0200

    class portinfo

--- getdata.py
+++ getdata.py
@@ -1,38 +1,28 @@
 from pysnmp.hlapi import *
 
-class SNMP_ClASS(object):
+class SNMP_PORTINFO_ClASS():
     """docstring for SNMP_CLASS."""
-    def __init__(self):
-        super(SNMP_CLASS, self).__init__()
+    def __init__(self, MIBOID_ARG):
+        #super(SNMP_CLASS, self).__init__()
         errorIndication, errorStatus, errorIndex, varBinds = next(
             getCmd(SnmpEngine(),
                    CommunityData('H5'),
                    UdpTransportTarget(('192.168.1.5', 161)),
                    ContextData(),
-
-errorIndication, errorStatus, errorIndex, varBinds = next(
-    getCmd(SnmpEngine(),
-           CommunityData('H5'),
-           UdpTransportTarget(('192.168.1.5', 161)),
-           ContextData(),
-           ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)),
-           ObjectType(ObjectIdentity('IF-MIB', 'ifNumber', 0)),
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.15.10101')),
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.8.5')),
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')))
+                   ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.'+ MIBOID_ARG)))
 )
-
-
-if errorIndication:
+        if errorIndication:
 
 
 
-    print(errorIndication)
+            print(errorIndication)
 
-elif errorStatus:
-    print('%s at %s' % (errorStatus.prettyPrint(),
-                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-else:
-    for varBind in varBinds:
-        print(' = '.join([x.prettyPrint() for x in varBind]))
-mib, mibindex, mibnumber
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+        else:
+            for varBind in varBinds:
+                print(' = '.join([x.prettyPrint() for x in varBind]))
+
+G1NAME = SNMP_PORTINFO_ClASS('2.10101')
+G1STAUS = SNMP_PORTINFO_ClASS('8.10101')
