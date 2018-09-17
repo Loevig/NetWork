36ffd98eb6a2e4ac5211a6729c5e07e5f0175f27
lasse9000dk <lasse9000dk@gmail.com>
2018-09-17 09:20:05 +0200

    asd

---
+++ Telnet.py
@@ -0,0 +1,17 @@
+import pexpect
+import getpass
+
+HOST = "192.168.1.5"
+user = raw_input("Enter your remote account: ")
+password = getpass.getpass()
+
+child = pexpect.spawn ('telnet '+HOST)
+child.expect ('Username: ')
+child.sendline (user)
+child.expect ('Password: ')
+child.sendline (password)
+# If the hostname of the router is set to "deep"
+# then the prompt now would be "deep>"
+routerHostname = "H5-prog" #example - can be different
+child.expect (routerHostname+'>')
+child.sendline ('enable')
