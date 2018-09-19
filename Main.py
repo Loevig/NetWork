b07ef41862dd32af6dc7e8eb0375f05e0084d838
Scarleck <loevig.inc@gmail.com>
2018-09-19 10:35:48 +0200

    asd

--- Main.py
+++ Main.py
@@ -1,4 +1,26 @@
-import getdata
-G1NAME = getdata.SNMP_PORTINFO_ClASS('2.10101')
-G1NAMESTR = str(G1NAME)
-print(G1NAMESTR)
+import getdataV2
+import pexpect
+import getpass
+G1NAME = getdataV2.Get_PortInfo()
+
+
+
+
+HOST = "192.168.1.5"
+#user = raw_input("Enter your remote account: ")
+password = 'cisco'
+
+child = pexpect.spawn('telnet '+HOST)
+#child.expect('Username: ')
+#child.sendline(user)
+child.expect('Password: ')
+child.sendline(password)
+# If the hostname of the router is set to "deep"
+# then the prompt now would be "deep>"
+routerHostname = "H5-prog"  # example - can be different
+child.expect(routerHostname+'>')
+child.sendline('enable')
+child.sendline(password)
+child.sendline ('conf t')
+child.sendline('int '+ G1G1NAME)
+child.sendline('sh')
