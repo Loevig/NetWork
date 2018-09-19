68a6fe3459fd02cced55b15b47fe4e83686757b6
lasse9000dk <lasse9000dk@gmail.com>
2018-09-19 10:43:39 +0200

    it works

--- getdataV2.py
+++ getdataV2.py
@@ -1,19 +1,19 @@
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 
-#cmdGen = cmdgen.CommandGenerator()
+# cmdGen = cmdgen.CommandGenerator()
 
 
 class Get_PortInfo():
-    cmdGen = cmdgen.CommandGenerator()
-    errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
-        cmdgen.CommunityData('H5'),
-        cmdgen.UdpTransportTarget(('192.168.1.5', 161)),
-        #cmdgen.ContextData(),
-        '1.3.6.1.2.1.2.2.1.2.10101',
-        lookupNames=True, lookupValues=True
-        )
+    def __init__(self):
+        cmdGen = cmdgen.CommandGenerator()
+        errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+            cmdgen.CommunityData('H5'),
+            cmdgen.UdpTransportTarget(('192.168.1.5', 161)),
+            # cmdgen.ContextData(),
+            '1.3.6.1.2.1.2.2.1.2.10101',
+            lookupNames=True, lookupValues=True
+            )
 
-
-    name = str(varBinds[0])
-    print(name[-18:])
+        name = str(varBinds[0])
+        self.name = (name[-18:])
